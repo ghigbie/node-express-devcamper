9ce256e525ef2b9a4b07bfd0ef95cf11664aab4d
ghigbie <gehigbie@gmail.com>
2020-11-15 18:56:36 -0500

    Modify var

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,16 +4,34 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc  Get all bootcamps
 // @route  GET /api/vi/bootcamp
 // @access Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, message: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try{
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({
+            success: true, 
+            data: bootcamps, 
+        })
+    }catch (err){
+        res.status(400).json({success: false});
+    }
 }
 
 
 // @desc  Get single bootcamp
 // @route  GET /api/vi/bootcamp/:id
 // @access Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, message: `Display bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    console.log(req.params.id);
+    try{
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        res.status(200).json({
+            success: true, 
+            data: bootcamp, 
+        });
+    }catch (err){
+        res.status(400).json({success: false, badID: req.params.id});
+    }
+        
 }
 
 // @desc   Create new bootcamp
