0d7c3208f2f4d9dd6b3f5c92d6794b31c6be2db1
ghigbie <gehigbie@gmail.com>
2020-11-14 23:41:32 -0500

    Add dummy data

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,11 @@
+const Colors = require('colors');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc  Get all bootcamps
 // @route  GET /api/vi/bootcamp
 // @access Public
 exports.getBootcamps = (req, res, next) => {
+    req.body ? console.log('Body: ', req.body.blue) : console.log('no body'.red);
     res.status(200).json({ success: true, message: 'Show all bootcamps' });
 }
 
